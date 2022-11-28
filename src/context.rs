9ed99e6ac94544adfede52caf20f0b96d4588930
Carlos Sanchez <randomouscrap98@aol.com>
2022-11-28 06:04:19 -0500

    Messy simplified request guard

--- src/context.rs
+++ src/context.rs
@@ -1,8 +1,9 @@
 
-use std::net::{IpAddr, SocketAddr};
+use std::net::IpAddr;
 
+use rocket::outcome::{try_outcome, IntoOutcome, Outcome};
 use reqwest::Client;
-use rocket::http::CookieJar;
+use rocket::{http::CookieJar, request::FromRequest};
 use crate::config::Config;
 
 //This is the request context, which rocket may have systems for but I don't want to deal with that
@@ -11,12 +12,12 @@ pub struct Context
     pub config: Config,
     pub client: Client,
     pub user_token: Option<String>,
-    pub client_ip: IpAddr 
+    pub client_ip: Option<IpAddr>
 }
 
 impl Context
 {
-    pub fn new<'a>(config: &Config, jar: &CookieJar<'_>, ip: IpAddr) -> Self
+    pub fn new<'a>(config: &Config, jar: &CookieJar<'_>, ip: Option<IpAddr>) -> Self
     {
         Self
         {
@@ -25,5 +26,39 @@ impl Context
             user_token: jar.get(&config.token_cookie_key).and_then(|cookie| Some(String::from(cookie.value()))),
             client_ip: ip
         }
+    }
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for Context {
+    type Error = std::convert::Infallible;
+
+    async fn from_request(request: &'r rocket::Request<'_>) -> rocket::request::Outcome<Self, Self::Error> {
+        //Assuming not expensive
+        //let config = try_outcome!(request.guard::<rocket::State<Config>>().await);
+        //let config = //request.rocket().state::<Config>().or_forward(());//request.guard::<&rocket::State<Config>>().await;
+        let jar = request.cookies(); //try_outcome!(request.guard::<&CookieJar<'_>>().await);
+        let ip = request.client_ip(); //try_outcome!(request.guard::<&CookieJar<'_>>().await);
+        
+        //I honestly don't know how to do this, I'm going crazy
+        if let Some(config) = request.rocket().state::<Config>() {
+            Outcome::Success(Context::new(config, jar, ip))
+        }
+        else {
+            //IDK, the example had it
+            Outcome::Forward(())//Failure("Couldn't pull config?")
+        }
+        //.map(|config| Context::new(config, jar, ip)).or_forward(())
+
+        //let context = request.local_cache_async(async {
+        //    let jar = request.guard::<&rocket::State<Config>>().await.succeeded()?;
+        //    request.cookies()
+        //        .get_private("user_id")
+        //        .and_then(|cookie| cookie.value().parse().ok())
+        //        .and_then(|id| db.get_user(id).ok())
+        //}).await;
+
+        //context.as_ref().or_forward(())
+        //rocket::outcome::Outcome::Success(Context::new(config, jar, ip))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -42,24 +42,24 @@ macro_rules! basic_template{
     }
 }
 
-//Generate a basic route with all the stupid cruft we need. Blegh, rocket is cool but it's very
-//verbose since it's all through functions. Maybe there's a better way?
-macro_rules! basic_route {
-    (
-        $route_meta:meta, $fn_name:ident ( $($field_name:ident : $field_type:ty),* ), $context:ident => $code:block
-    ) => {
-        basic_route!{$route_meta, $fn_name($($field_name:$field_type)*), jar, $context => $code}
-    };
-    (
-        $route_meta:meta, $fn_name:ident ( $($field_name:ident : $field_type:ty),* ), $jar:ident, $context:ident => $code:block
-    ) => {
-        #[$route_meta] //If we need more meta, oh well, fix it later
-        async fn $fn_name(config: &State<config::Config>, $jar: &CookieJar<'_>, remote_ip: IpAddr, $($field_name:$field_type)*) -> Result<MultiResponse, RocketCustom<String>> {
-            let $context = context::Context::new(config, $jar, remote_ip);
-            $code
-        }
-    };
-}
+////Generate a basic route with all the stupid cruft we need. Blegh, rocket is cool but it's very
+////verbose since it's all through functions. Maybe there's a better way?
+//macro_rules! basic_route {
+//    (
+//        $route_meta:meta, $fn_name:ident ( $($field_name:ident : $field_type:ty),* ), $context:ident => $code:block
+//    ) => {
+//        basic_route!{$route_meta, $fn_name($($field_name:$field_type)*), jar, $context => $code}
+//    };
+//    (
+//        $route_meta:meta, $fn_name:ident ( $($field_name:ident : $field_type:ty),* ), $jar:ident, $context:ident => $code:block
+//    ) => {
+//        #[$route_meta] //If we need more meta, oh well, fix it later
+//        async fn $fn_name(config: &State<config::Config>, $jar: &CookieJar<'_>, remote_ip: IpAddr, $($field_name:$field_type)*) -> Result<MultiResponse, RocketCustom<String>> {
+//            let $context = context::Context::new(config, $jar, remote_ip);
+//            $code
+//        }
+//    };
+//}
 
 #[derive(Debug, Responder)]
 pub enum MultiResponse {
@@ -67,19 +67,23 @@ pub enum MultiResponse {
     Redirect(Redirect),
 }
 
-basic_route!{ get("/"), index_get(), context => {
-    Ok(MultiResponse::Template(basic_template!("index", context, {})))
-}}
+#[get("/")]
+async fn index_get(context: context::Context) -> Result<Template, RocketCustom<String>> {
+    Ok(basic_template!("index", context, {}))
+}
 
-basic_route!{ get("/login"), login_get(), context => {
-    Ok(MultiResponse::Template(basic_template!("login", context, {})))
-}}
+#[get("/login")]
+async fn login_get(context: context::Context) -> Result<Template, RocketCustom<String>> {
+    Ok(basic_template!("login", context, {}))
+}
 
-basic_route!{ get("/userhome"), userhome_get(), context => {
-    Ok(MultiResponse::Template(basic_template!("userhome", context, {})))
-}}
+#[get("/userhome")]
+async fn userhome_get(context: context::Context) -> Result<Template, RocketCustom<String>> {
+    Ok(basic_template!("userhome", context, {}))
+}
 
-basic_route!{ post("/login", data = "<login>"), login_post(login: Form<forms::Login<'_>>), jar, context => {
+#[post("/login", data = "<login>")]
+async fn login_post(context: context::Context, login: Form<forms::Login<'_>>, jar: &CookieJar<'_>) -> Result<MultiResponse, RocketCustom<String>> {
     match api::post_login(&context, &login).await
     {
         Ok(result) => {
@@ -92,7 +96,33 @@ basic_route!{ post("/login", data = "<login>"), login_post(login: Form<forms::Lo
             Ok(MultiResponse::Template(basic_template!("login", context, {errors: vec![error.get_just_string()]})))
         } 
     }
-}}
+}
+//basic_route!{ get("/"), index_get(), context => {
+//    Ok(MultiResponse::Template(basic_template!("index", context, {})))
+//}}
+
+//basic_route!{ get("/login"), login_get(), context => {
+//    Ok(MultiResponse::Template(basic_template!("login", context, {})))
+//}}
+//
+//basic_route!{ get("/userhome"), userhome_get(), context => {
+//    Ok(MultiResponse::Template(basic_template!("userhome", context, {})))
+//}}
+
+//basic_route!{ post("/login", data = "<login>"), login_post(login: Form<forms::Login<'_>>), jar, context => {
+//    match api::post_login(&context, &login).await
+//    {
+//        Ok(result) => {
+//            //Again with the wasting memory and cpu, it's whatever. If we needed THAT much optimization,
+//            //uhh... well we'd have a lot of other problems than just a single small key copy on the heap
+//            jar.add(Cookie::new(context.config.token_cookie_key.clone(), result));
+//            Ok(MultiResponse::Redirect(my_redirect!(context.config, "/")))
+//        },
+//        Err(error) => {
+//            Ok(MultiResponse::Template(basic_template!("login", context, {errors: vec![error.get_just_string()]})))
+//        } 
+//    }
+//}}
 
 //Don't need the heavy lifting of an entire context just for logout 
 #[get("/logout")]
