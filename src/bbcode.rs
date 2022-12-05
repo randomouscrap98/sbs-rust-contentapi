280770872b2f99b1e49ae1ef26776a8bbf5c929a
Carlos Sanchez <randomouscrap98@aol.com>
2022-12-05 04:29:07 -0500

    added default bbcode tags

--- src/bbcode.rs
+++ src/bbcode.rs
@@ -9,7 +9,7 @@ use regex::Regex;
 pub struct TagInfo {
     //The tag identity, such as "b", "youtube", etc
     pub tag : &'static str,
-    pub argout : &'static str,
+    pub argout : Option<&'static str>,
     pub verbatim : bool //whether tags are allowed inside, basically
 }
 
@@ -37,7 +37,9 @@ pub struct BBCode {
 }
 
 impl BBCode {
-    //Maybe get rid of anyhow if you want to separate this, kind of a big thing to include
+    //Maybe get rid of anyhow if you want to separate this, kind of a big thing to include.
+    //Anyway, this build function precompiles all the regex for you. Try to reuse this item
+    //as much as possible, it doesn't require mutation and so can be part of the global rocket state
     pub fn build(taginfos: Vec<TagInfo>) -> Result<Self, anyhow::Error> {
         //First, get the default direct replacements
         let mut tags = Self::html_escapes().iter().map(|e| {
@@ -49,7 +51,7 @@ impl BBCode {
             })
         }).collect::<Result<Vec<TagDo>,anyhow::Error>>()?;
 
-        //Next, convert the taginfos to even more "do"
+        //Next, convert the taginfos to even more "do".
         for tag in taginfos.iter() {
             //The existing system on SBS doesn't allow spaces in tags at ALL. I don't know if this 
             //much leniency on the = value is present in the old system though...
@@ -61,7 +63,7 @@ impl BBCode {
             let close_tag = format!(r#"^\[/{}\]"#, tag.tag);
             tags.push(TagDo {
                 regex: Regex::new(&close_tag)?,
-                match_type : MatchType::Open(tag.clone())
+                match_type : MatchType::Close(tag.clone())
             });
         }
 
@@ -78,6 +80,20 @@ impl BBCode {
         ]
     }
 
+    //Get a vector of the basic taginfos of bbcode
+    pub fn basics() -> Vec<TagInfo> {
+        vec![
+            TagInfo { tag: "b", argout: None, verbatim: false },
+            TagInfo { tag: "i", argout: None, verbatim: false },
+            TagInfo { tag: "sup", argout: None, verbatim: false },
+            TagInfo { tag: "sub", argout: None, verbatim: false },
+            TagInfo { tag: "url", argout: None, verbatim: true },
+            TagInfo { tag: "img", argout: None, verbatim: true },
+            TagInfo { tag: "s", argout: None, verbatim: false },
+            TagInfo { tag: "u", argout: None, verbatim: false }
+        ]
+    }
+
     pub fn parse(&self, input: String) -> String {
         //We know it will be at LEAST as big, and that strings usually double in size
         //when they grow anyway, so just start at 2X by default
@@ -86,9 +102,11 @@ impl BBCode {
         //Because of utf-8, it's better to just use regex directly all the time?
         let slice = &input[0..]; //Not necessary to be this explicit ofc
 
-        let mut scopes : Vec<&str> = Vec::new();
+        //Only 'Taginfo' can create scope, so don't worry about "DirectReplace" types
+        let mut scopes : Vec<&TagInfo> = Vec::new();
 
-        //While there is string
+        //While there is string left, keep checking against all the regex. Remove some regex
+        //if the current scope is a meanie
         while slice.len() > 0
         {
 
