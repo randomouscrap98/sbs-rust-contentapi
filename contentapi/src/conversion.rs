ff3aafb0928f4688706b31f9a5466a3a767da6bd
Carlos Sanchez <randomouscrap98@aol.com>
2022-12-23 22:37:15 -0500

    Ugh left one file off

--- contentapi/src/conversion.rs
+++ contentapi/src/conversion.rs
@@ -5,8 +5,7 @@ use serde::Deserialize;
 //Some data can't be used as-is. In those cases, we must translate from frontend formats to
 //backend formats
 
-//pub fn cast_result<T>(result: &api_data::RequestResult, name: &str) -> Result<Option<Vec<T>>, Box<dyn Error>> where T: for<'a> Deserialize<'a>  
-//Cast result; if key doesn't exist, you get none.
+/// Cast result; if key doesn't exist, you get none.
 pub fn cast_result<T>(result: &RequestResult, name: &str) -> Result<Option<Vec<T>>, Box<dyn std::error::Error>> where T: for<'a> Deserialize<'a>  
 {
     //If the key exists, do the conversion
@@ -23,26 +22,29 @@ pub fn cast_result<T>(result: &RequestResult, name: &str) -> Result<Option<Vec<T
 
 }
 
-//Cast result without care if the key exists. You'll get an empty vector
+/// Cast result without care if the key exists. You'll get an empty vector
 pub fn cast_result_safe<T>(result: &RequestResult, name: &str) -> Result<Vec<T>, Box<dyn std::error::Error>> where T: for<'a> Deserialize<'a>  
 {
     cast_result(result, name).and_then(|r| Ok(r.unwrap_or(Vec::new())))
 }
 
-//Cast result but throw error if the key isn't found
+/// Cast result but throw error if the key isn't found
 pub fn cast_result_required<T>(result: &RequestResult, name: &str) -> Result<Vec<T>, Box<dyn std::error::Error>> where T: for<'a> Deserialize<'a>  
 {
     cast_result(result, name)?.ok_or(format!("Couldn't find key {}", name).into())
 }
 
+/// Convert a vector of users into a hashmap (id is key)
 pub fn map_users(users: Vec<User>) -> HashMap<i64, User> {
     users.into_iter().map(|u| (u.id, u)).collect::<HashMap<i64, User>>()
 }
 
+/// Convert a vector of content into a hashmap (id is key)
 pub fn map_content(content: Vec<Content>) -> HashMap<i64, Content> {
     content.into_iter().map(|u| (u.id.unwrap_or_else(|| 0), u)).collect::<HashMap<i64, Content>>()
 }
 
+/// Convert a vector of messages into a hashmap (id is key)
 pub fn map_messages(messages: Vec<Message>) -> HashMap<i64, Message> {
     messages.into_iter().map(|u| (u.id.unwrap_or_else(|| 0), u)).collect::<HashMap<i64, Message>>()
 }
