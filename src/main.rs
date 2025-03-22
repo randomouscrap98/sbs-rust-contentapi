f8f5e6762148290617f79d9ce1c7e9a494e74a18
haloopdy <randomouscrap98@aol.com>
2025-03-22 06:01:34 -0400

    Small updates

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,12 @@ mod state;
 
 use crate::state::*;
 
-#[cfg(not(target_env = "msvc"))]
-use tikv_jemallocator::Jemalloc;
-
-#[cfg(not(target_env = "msvc"))]
-#[global_allocator]
-static GLOBAL: Jemalloc = Jemalloc;
+// #[cfg(not(target_env = "msvc"))]
+// use tikv_jemallocator::Jemalloc;
+//
+// #[cfg(not(target_env = "msvc"))]
+// #[global_allocator]
+// static GLOBAL: Jemalloc = Jemalloc;
 
 static CONFIGNAME: &str = "settings";
 
